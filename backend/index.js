1d73654dde14055aac2cb4e25e4ed8f36b2f3f2d
Stanislav Hamara <stanislav.hamara@arm.com>
2022-03-13 10:15:35 +0000

    GET point for products

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,38 @@
 var express = require('express');
-var fs = require("fs");
+const { Pool, Client } = require("pg");
+
+const credentials = {
+    user: "postgres",
+    host: "localhost",
+    database: "xboxdatabase",
+    password: "pwd",
+    port: 5432,
+};
+
+async function clientDemo() {
+    const client = new Client(credentials);
+    await client.connect();
+    const now = await client.query("SELECT NOW()");
+    await client.end();
+
+    return now;
+}
 
 var app = express();
 
-app.get('/products', function (req, res) {
-    res.end(JSON.stringify({ data: "someData" }))
+app.get('/products', async function (req, res) {
+    const pool = new Pool(credentials);
+    const q = `SELECT * FROM store`;
+    const result = await pool.query(q);
+    await pool.end()
+    res.end(JSON.stringify({ data: result.rows }))
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+var server = app.listen(8081, async function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+
+    const clientResult = await clientDemo();
+    console.log("Time with client: " + clientResult.rows[0]["now"]);
 })
