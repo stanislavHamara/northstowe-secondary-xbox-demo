a49fd0e28ce7a00ea0b90441609f77ca318d36b6
Stanislav Hamara <stanislav.hamara@arm.com>
2022-03-13 11:51:00 +0000

    More scripts for demo

---
+++ data/04_INSERT_ANOTHER_GAME.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    public.store(name, genre, rating, price, thumbnail)
+VALUES
+    (
+        'Halo Infinite',
+        'FPS',
+        4,
+        43.99,
+        'https://store-images.s-microsoft.com/image/apps.21536.13727851868390641.c9cc5f66-aff8-406c-af6b-440838730be0.68796bde-cbf5-4eaa-a299-011417041da6'
+    )
---
+++ data/05_DELETE_GAME.sql
@@ -0,0 +1,4 @@
+DELETE FROM
+    store
+WHERE
+    genre LIKE 'FPS'
